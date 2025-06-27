49342b2503d7508be48522e1b794ad0d91dd9bd3
ASAI Haruhiko <s1320047@u-aizu.ac.jp>
2025-06-27 11:04:00 +0900

    Dice game: initial revision

---
+++ DiceGame.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+    srand((unsigned int)time(NULL)); 
+    int d[2];
+    int total = 0;
+
+    printf("Rolling dice...\n");
+    for(int i = 0 ; i < 2 ; i++){
+        d[i] = rand() % 7;
+        total += d[i];
+    }
+    for(int i = 0 ; i < 2 ; i++){
+        printf("Die %d: %d\n",i+1,d[i]);
+    }
+    printf("Total value: %d\n", total);
+
+    return 0;
+}
